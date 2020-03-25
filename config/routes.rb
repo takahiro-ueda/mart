eea0e8857a1fafb16fe8c245b4a518e0e900268a
takahiro-ueda <pt25ue@gmail.com>
2020-03-25 23:30:01 +0900

    クレジット購入機能１

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
   get 'purchase/index'
   get 'purchase/done'
-  get 'credit/new'
-  get 'credit/show'
-  # devise_for :users, controllers: {
-  #   registrations: 'users/registrations'
-  # }
-  # devise_scope :user do
-  #   get 'addresses', to: 'users/registrations#new_address'
-  #   post 'addresses', to: 'users/registrations#create_address'
-  # end
-  # root to: "home#index"
-  root to: "credit#new"
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  root to: "home#index"
+  # root to: "credit#new"
 
   
-  resources :card, only: [:new, :show] do
+  resources :credit, only: [:new, :show] do
     collection do
-      post 'show', to: 'card#show'
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      post 'show', to: 'credit#show'
+      post 'pay', to: 'credit#pay'
+      post 'delete', to: 'credit#delete'
     end
   end
 end
